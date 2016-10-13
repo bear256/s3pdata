a327155242e789432c7c95bfe74b40c58278951a
Peter Pan <bear256@qq.com>
2016-10-13 17:15:53 +0800

    Remove final directory

--- final/twitterraw1.bat
+++
@@ -1,3 +0,0 @@
-set JAVA_HOME=D:\Program Files (x86)\Java\jdk1.8.0_73
-set PATH=%JAVA_HOME%\bin;%PATH%
-java -Djava.net.preferIPv4Stack=true -jar RawJob.jar s3pdata1.properties twitter
--- final/twittertotal1.bat
+++
@@ -1,4 +0,0 @@
-set JAVA_HOME=D:\Program Files\Java\jdk1.8.0_73
-set PATH=%JAVA_HOME%\bin;%PATH%
-java -version
-java -Djava.net.preferIPv4Stack=true -jar TwitterTotalJob.jar s3pdata1.properties twitter
