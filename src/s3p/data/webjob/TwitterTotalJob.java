b48797d7dd1fc9568f5d57471d17ef0cd833a2fe
Peter Pan <bear256@qq.com>
2016-10-26 13:02:45 +0800

    Update

--- src/s3p/data/webjob/TwitterTotalJob.java
+++ src/s3p/data/webjob/TwitterTotalJob.java
@@ -24,7 +24,7 @@ public class TwitterTotalJob {
 		JobEntity job = TableUtils.readJob(jobName);
 		DateTime start = new DateTime(job.getStart());
 		DateTime end = DateTime.now(TimeZone.getTimeZone("GMT+0"));
-		DateTime cur = new DateTime(job.getLast()).minusDays(1);
+		DateTime cur = new DateTime(job.getLast());
 		while (cur.lteq(end)) {
 			System.out.println(cur);
 			// Hourly
