c9a10c6c80d6b0e9a7ec12b59cebc881a7fe7374
Peter Pan <bear256@qq.com>
2016-10-28 18:05:15 +0800

    Add Class for ImpactSummary Endpoint

---
+++ src/s3p/data/endpoint/impactsummary/MostMentionedService.java
@@ -0,0 +1,37 @@
+package s3p.data.endpoint.impactsummary;
+
+import s3p.data.endpoint.mentionedmost.MentionedMostService;
+
+public class MostMentionedService {
+	private MentionedMostService mentionedmostservice;
+	private Integer occupyratio;
+
+	public MostMentionedService() {
+		super();
+		this.mentionedmostservice = new MentionedMostService();
+		this.occupyratio = 0;
+	}
+
+	public MostMentionedService(MentionedMostService mentionedmostservice, Integer occupyratio) {
+		super();
+		this.mentionedmostservice = mentionedmostservice;
+		this.occupyratio = occupyratio;
+	}
+
+	public MentionedMostService getMentionedmostservice() {
+		return mentionedmostservice;
+	}
+
+	public void setMentionedmostservice(MentionedMostService mentionedmostservice) {
+		this.mentionedmostservice = mentionedmostservice;
+	}
+
+	public Integer getOccupyratio() {
+		return occupyratio;
+	}
+
+	public void setOccupyratio(Integer occupyratio) {
+		this.occupyratio = occupyratio;
+	}
+
+}
